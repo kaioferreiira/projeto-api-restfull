dc2fa3160141335f4f4ed36ba090f0720b609446
kaioferreira <kaioferreiracosta@hotmail.com>
2018-08-19 18:05:59 -0300

    Arquivos iniciais: Exemplo Controller

---
+++ projeto-api-restfull/src/main/java/br/com/api/restfull/service/ExemploService.java
@@ -0,0 +1,11 @@
+package br.com.api.restfull.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExemploService {
+
+	public void testarServico() {
+		System.out.println("### Executando serviço de teste!!!");
+	}
+}
